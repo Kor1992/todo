c1d0b542bfe36ce481feac9a92245153452523dd
Tim <fja6578@gmail.com>
2026-03-21 21:40:57 +0300

    work environment11

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,8 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS todoapp;
 
 CREATE TABLE todoapp.users (
-    id           SERIAL       PRIMARY KEY,
-    version      BIGINT       NOT NULL DEFAULT 1,
+    id           SERIAL PRIMARY KEY,
+    version      BIGINT NOT NULL DEFAULT 1,
     full_name    VARCHAR(100) NOT NULL CHECK (char_length(full_name) BETWEEN 3 AND 100),
-    phone_number VARCHAR(15)           CHECK (phone_number ~ '^\+?[0-9]+$' AND (char_length(phone_number) BETWEEN 10 AND 15)
+    phone_number VARCHAR(15) CHECK (
+        phone_number ~ '^\+?[0-9]+$' 
+        AND char_length(phone_number) BETWEEN 10 AND 15
+    )
 );
