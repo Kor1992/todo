5d03bcf3e8635d019dee5ccf8ce88ecbc0559a60
Tim <fja6578@gmail.com>
2026-03-23 11:53:03 +0300

    transport

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	core_logger "github.com/Kor1992/todo/internal/core/logger"
+	core_http_server "github.com/Kor1992/todo/internal/core/transport/http/server"
+	users_transport_http "github.com/Kor1992/todo/internal/features/users/transport/http"
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT, syscall.SIGTERM,
+	)
+	defer cancel()
+
+	usersTransportHTTP := users_transport_http.NewUsersHTTPHandler(nil)
+	log, err := core_logger.NewLogger(core_logger.NewConfigMust())
+	if err != nil {
+		fmt.Println("failed to init application logger:", err)
+		os.Exit(1)
+	}
+	defer log.Close()
+	log.Debug("Starting ToDo app")
+
+	usersRouters := usersTransportHTTP.Routers()
+
+	apiVersionRouter := core_http_server.NewApiVersionRouter(core_http_server.ApiVersion1)
+	apiVersionRouter.RegisterRoutes(usersRouters...)
+
+	httpServer := core_http_server.NewHTTPServer(core_http_server.NewConfigMust(), log)
+
+	httpServer.RegisterAPIRouter(apiVersionRouter)
+
+	if err := httpServer.Run(ctx); err != nil {
+		log.Error("HTTP server run error", zap.Error(err))
+	}
+
 }
--- internal/core/transport/http/middleware/common.go
+++ internal/core/transport/http/middleware/common.go
@@ -3,8 +3,10 @@ package core_middleware
 import (
 	"context"
 	"net/http"
+	"time"
 
 	core_logger "github.com/Kor1992/todo/internal/core/logger"
+	core_http_response "github.com/Kor1992/todo/internal/core/transport/http/response"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
@@ -32,11 +34,55 @@ func Logger(log *core_logger.Logger) MiddleWare {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			requestId := r.Header.Get(requestIDHeader)
 
-			l := log.With(zap.String("request_id", requestId), zap.String("url", r.URL.String()))
+			l := log.With(
+				zap.String("request_id", requestId),
+				zap.String("url", r.URL.String()),
+			)
 
 			ctx := context.WithValue(r.Context(), "log", l)
 
 			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+func Panic() MiddleWare {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			ctx := r.Context()
+			log := core_logger.FromContext(ctx)
+			responseHAndler := core_http_response.NewHTTEPResponseHandler(log, w)
+			defer func() {
+				if p := recover(); p != nil {
+					responseHAndler.PanicResponse(p, "during handle HTTP request got unexpected pnic")
+				}
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func Trace() MiddleWare {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			log := core_logger.FromContext(ctx)
+			rw := core_http_response.NewResponseWriter(w)
+
+			before := time.Now()
+			log.Debug(
+				">>> incoming HTTP request",
+				zap.Time("time: ", before.UTC()),
+			)
+
+			next.ServeHTTP(rw, r)
+
+			log.Debug(
+				"<<< done HTTP request",
+				zap.Int("status_code", rw.GetStatusCodeOrPanic()),
+				zap.Duration("latency: ", time.Since(before)),
+			)
 		})
 	}
 }
